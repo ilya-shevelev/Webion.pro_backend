cf9d93b9a416014fc3fd4e6fda72eacc750a4242
Ilya <ilasevelev10776@gmail.com>
2023-02-26 21:14:42 +0300

    add STATIC_ROOT and staticfiles directory

--- config/settings.py
+++ config/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATIC_DIR = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_URL = "/media/"
